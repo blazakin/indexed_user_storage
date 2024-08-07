ee0a64d90770e62fea67be8b6d27b440f6efb1ef
blazakin <felschf05@gmail.com>
2024-08-06 23:14:27 -0700

    Python helper functions for client using the microservice.

---
+++ client_storage_helper.py
@@ -0,0 +1,41 @@
+import zmq
+
+
+def send_storgage_message(message):
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5551")
+
+    socket.send_string(message)
+
+    message = socket.recv()
+    return message.decode()
+
+
+def end_storage_server():
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("tcp://localhost:5551")
+    socket.send_string("Exit")
+
+
+def storage_check_user(user):
+    return send_storgage_message(f"Option: Check User, User: {user}, History: NA")
+
+
+def storage_add_user(user):
+    return send_storgage_message(f"Option: Add User, User: {user}, History: NA")
+
+
+def storage_add_history(user, history):
+    return send_storgage_message(f"Option: Add History, User: {
+        user}, History: {history}")
+
+
+def storage_get_history(user):
+    return send_storgage_message(f"Option: Get History, User: {user}, History: NA")
+
+
+def storage_get_history_by_index(user, index):
+    return send_storgage_message(f"Option: Get History By Index, User: {
+        user}, History: {index}")
