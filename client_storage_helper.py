efa650346df99f1ef4ed43c90c8e6c170a1d300e
blazakin <felschf05@gmail.com>
2024-08-12 21:54:47 -0700

    Fixed function name typo

--- client_storage_helper.py
+++ client_storage_helper.py
@@ -1,7 +1,7 @@
 import zmq
 
 
-def send_storgage_message(message):
+def send_storage_message(message):
     context = zmq.Context()
     socket = context.socket(zmq.REQ)
     socket.connect("tcp://localhost:5551")
@@ -20,22 +20,22 @@ def end_storage_server():
 
 
 def storage_check_user(user):
-    return send_storgage_message(f"Option: Check User, User: {user}, History: NA")
+    return send_storage_message(f"Option: Check User, User: {user}, History: NA")
 
 
 def storage_add_user(user):
-    return send_storgage_message(f"Option: Add User, User: {user}, History: NA")
+    return send_storage_message(f"Option: Add User, User: {user}, History: NA")
 
 
 def storage_add_history(user, history):
-    return send_storgage_message(f"Option: Add History, User: {
+    return send_storage_message(f"Option: Add History, User: {
         user}, History: {history}")
 
 
 def storage_get_history(user):
-    return send_storgage_message(f"Option: Get History, User: {user}, History: NA")
+    return send_storage_message(f"Option: Get History, User: {user}, History: NA")
 
 
 def storage_get_history_by_index(user, index):
-    return send_storgage_message(f"Option: Get History By Index, User: {
+    return send_storage_message(f"Option: Get History By Index, User: {
         user}, History: {index}")
